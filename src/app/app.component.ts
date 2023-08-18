84b9a2cf7a1cc3fa7679bb34e5c811a9ec8d169b
sankarkalimuthu <sankar.kalimuthu@laboneft.com>
2023-08-18 15:29:11 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'application';
+  inputNumber:number=1;
+  tableData:Map<number,number[]> =new Map();
+  sum:number[]=[];
+  average:number[]=[];
+  index:any=[]
+ 
+  onSubmit(){
+    this.index=new Array(this.inputNumber);
+    for(let i=1;i<=this.inputNumber;i++){
+      let val:number[]=[]
+      for(let j=1;j<=this.inputNumber;j++){
+          val.push(i*j);
+      }
+      this.tableData.set(i,val);
+      let sumOfRow:number=val.reduce((sum, num) => sum + num, 0);
+      let averageOfRow:number=sumOfRow/this.inputNumber;
+      this.sum.push(sumOfRow);
+      this.average.push(averageOfRow);
+    }
+    console.log(this.tableData);
+  }
 }
