60f59cdc446af6ca55479032f2ca5d9914c75dfe
sankarkalimuthu <sankar.kalimuthu@laboneft.com>
2023-08-18 17:07:11 +0530

    comments added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,20 +14,21 @@ export class AppComponent {
   index:any=[]
  
   onSubmit(){
-    if(this.inputNumber<1){
+    if(this.inputNumber<1){ //validating input
       alert("Enter positive numbers only");
       return;
     }
   
-    this.index=new Array(this.inputNumber);
+    this.index=new Array(this.inputNumber); //initializeing empty array for indexing;
+
     for(let i=1;i<=this.inputNumber;i++){
       let val:number[]=[]
       for(let j=1;j<=this.inputNumber;j++){
           val.push(i*j);
       }
-      this.tableData.set(i,val);
-      let sumOfRow:number=val.reduce((sum, num) => sum + num, 0);
-      let averageOfRow:number=sumOfRow/this.inputNumber;
+      this.tableData.set(i,val);  //storing the key , value pair of index and values of table
+      let sumOfRow:number=val.reduce((sum, num) => sum + num, 0); //calculating sum of the values;
+      let averageOfRow:number=sumOfRow/this.inputNumber; //calculating average of the values
       this.sum.push(sumOfRow);
       this.average.push(averageOfRow);
     }
