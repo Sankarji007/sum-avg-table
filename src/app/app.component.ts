c3bea7aa63b59bdc5bc3d2dbefadfafe9ba42b33
sankarkalimuthu <sankar.kalimuthu@laboneft.com>
2023-08-18 16:00:45 +0530

    alert message added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'application';
-  inputNumber:number=1;
+  inputNumber:number=0;
   tableData:Map<number,number[]> =new Map();
   sum:number[]=[];
   average:number[]=[];
   index:any=[]
  
   onSubmit(){
+    if(this.inputNumber<1){
+      alert("Enter positive numbers only");
+      return;
+    }
+  
     this.index=new Array(this.inputNumber);
     for(let i=1;i<=this.inputNumber;i++){
       let val:number[]=[]
